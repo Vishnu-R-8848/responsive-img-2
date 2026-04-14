367576c8374a5c364c5fe577fd27786a1c87ce8f
Vishnu R <vishnu6364748848@gmail.com>
2026-04-14 13:07:40 +0530

    vercle bug fix

--- src/components/MainWrapper.jsx
+++ src/components/MainWrapper.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Hero from "./Hero";
-import Product from "./product";
+
 import Presentation from "./Presentation";
 import Contact from "./contact";
+import Product from "./product";
 
-const Main = () => {
+const MainWrapper = () => {
   return (
     <main className="main-wrapper h-full">
       <Hero />
       <Product />
       <Presentation />
-      <Contact/>
+      <Contact />
     </main>
   );
 };
 
-export default Main;
+export default MainWrapper;
